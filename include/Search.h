62665ee562a63d003da1797d45cdd84e627bf2e7
Alexander Figueiras <figueiras.alexander@gmail.com>
2024-07-18 17:02:20 -0400

    Fixed readme and cleaned unnecessary files

--- include/Search.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "resource.h"
-#include "framework.h"
-
-INT_PTR CALLBACK WndSearchProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
--- include/Toolbox.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-std::wstring const toWString(const std::string& str);
-std::string const toString(const std::wstring& str);
-
